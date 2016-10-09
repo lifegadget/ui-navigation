d9e698baf5bcfa4af044b40a0b63f380a6114bca
Ken Snyder <ken@ken.net>
2016-10-09 18:37:58 +0100

    chalk to "dep"

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,3 @@
 /// <reference path="globals/bootstrap/index.d.ts" />
-/// <reference path="globals/chalk/index.d.ts" />
 /// <reference path="globals/node-sass/index.d.ts" />
 /// <reference path="globals/showdown/index.d.ts" />
